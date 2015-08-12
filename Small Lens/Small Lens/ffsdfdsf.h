d6eab6a49d9e5bb791ee5b7b59f8da04e595ad08
huqichao1221 <1269596466@qq.com>
2015-08-12 11:29:20 +0800

    增加ffsd

---
+++ Small Lens/Small Lens/ffsdfdsf.h
@@ -0,0 +1,13 @@
+//
+//  ffsdfdsf.h
+//  Small Lens
+//
+//  Created by xzh on 15/8/12.
+//  Copyright (c) 2015年 zjm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ffsdfdsf : UIView
+
+@end
---
+++ Small Lens/Small Lens/ffsdfdsf.m
@@ -0,0 +1,13 @@
+//
+//  ffsdfdsf.m
+//  Small Lens
+//
+//  Created by xzh on 15/8/12.
+//  Copyright (c) 2015年 zjm. All rights reserved.
+//
+
+#import "ffsdfdsf.h"
+
+@implementation ffsdfdsf
+
+@end
