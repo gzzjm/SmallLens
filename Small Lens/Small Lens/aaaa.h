d238ea3bbd71dbed5823bef17b42c32fbcdb9e8b
JM Teo <753331342@qq.com>
2015-08-12 11:21:29 +0800

    增加aaa

---
+++ Small Lens/Small Lens/aaaa.h
@@ -0,0 +1,13 @@
+//
+//  aaaa.h
+//  Small Lens
+//
+//  Created by zjm on 15-8-12.
+//  Copyright (c) 2015年 zjm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface aaaa : UITabBarController
+
+@end
---
+++ Small Lens/Small Lens/aaaa.m
@@ -0,0 +1,37 @@
+//
+//  aaaa.m
+//  Small Lens
+//
+//  Created by zjm on 15-8-12.
+//  Copyright (c) 2015年 zjm. All rights reserved.
+//
+
+#import "aaaa.h"
+
+@interface aaaa ()
+
+@end
+
+@implementation aaaa
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
